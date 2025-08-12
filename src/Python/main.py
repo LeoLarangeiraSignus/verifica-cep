0f6049426f11bf32b61a0d44b17637b6a26dcd95
LeoLarangeiraSignus <leonardo.larangeira@signus.ind.br>
2025-08-12 16:46:20 -0300

    Study

--- src/Python/main.py
+++ src/Python/main.py
@@ -2,33 +2,54 @@ import requests
 import time
 import json
 
-# cep = "13260000"
 ceps = [
-        '13253322',
-        '07232-11',
-        '88307322',
-        '09823000',
-        '13347120',
-        '13347120',
-        '13080150',
-        '09370800',
-        '04795100',
-        '13260000']
+        {
+            'email': 'teste@mail.com',
+            'cep':  '13253322'},
+        {
+         'email': 'teste@mail.com',    
+        'cep': '07232-11'},
+        { 
+            'email': 'teste@mail.com',
+            'cep':'88307322'},
+        { 
+            'email': '',
+            'cep':'09823000'},
+        { 
+            'email': 'teste@mail.com',
+            'cep':'13347120'},
+        { 
+            'email': 'teste@mail',
+            'cep':'13347120'},
+        { 
+            'email': 'teste@mail.com',
+            'cep':'13080150'},
+        { 
+            'email': 'teste@mail.com',
+            'cep':'09370800'},
+        { 
+            'email': 'teste@mail.com',
+            'cep':'04795100'},
+        {
+            'email': 'leonardo.larangeira@signus.ind.br',
+            'cep':'13260000'}]
 erroCep = []
+email = ''
+aEmail = []
 # url = f"https://viacep.com.br/ws/{cep}/json/"
 
 
 for cep in range(len(ceps)):
-    res = requests.get(f'https://viacep.com.br/ws/{ceps[cep]}/json/')
-    time.sleep(1)
+    res = requests.get(f'https://viacep.com.br/ws/{ceps[cep]['cep']}/json/')
+    # time.sleep(1)
     if res.status_code == 200:
         dados = res.json()
-        # dados = json.loads(dados)
-        print(dados)
-        # print(dados['erro'])
         if 'erro' in dados:
             erroCep.append(ceps[cep])
-
+    for x in range(len(erroCep)):
+        email = erroCep[x]['email']
+        aEmail.append(email)
+print(email)
 print(erroCep)
 
 
