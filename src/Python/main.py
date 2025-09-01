4ac546a4f477b380091f87481de10991c4376248
LeoLarangeiraSignus <leonardo.larangeira@signus.ind.br>
2025-09-01 11:32:39 -0300

    consuming an xlsx file

--- src/Python/main.py
+++ src/Python/main.py
@@ -1,18 +1,32 @@
 import json
 import os
-
+import openpyxl
+import pandas as pd
 from ver import *
 
+
 base_path = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(base_path, "ceps.json")
+parent_path = os.path.dirname(base_path)
 
-#chamada do arquivo que contem os dados com Json
-'''
-Como eu não sei se posso conectar o Banco direto no projeto em python, crie um json com os 10 primeiros códigos 
-'''
+file_path = os.path.join(parent_path, "data","dados.xlsx")
 
-with open(file_path, "r") as f:
-    ceps = json.load(f)
-    for x in range(len(ceps)):
-        verifica_cep(ceps[x])
-    f.close()
+
+df = pd.read_excel(file_path)
+
+
+
+for index, row in df.iterrows():
+    name = row['A1_NOME']
+    email = row['A1_EMAIL']
+    tel = row['A1_TEL']
+    cep = row['A1_CEP']
+
+    ceps = {'nome': name, 'email': email, 'cep' : cep, 'tel' : tel}
+
+    
+    verifica_cep(ceps)
+# with open(file_path, "r") as f:
+#     ceps = json.load(f)
+#     for x in range(len(ceps)):
+#         verifica_cep(ceps[x])
+#     f.close()
--- src/Python/ver.py
+++ src/Python/ver.py
@@ -2,32 +2,33 @@ import requests
 import openpyxl
 from openpyxl.styles import Font, Alignment, PatternFill
 import os
-import pprint
+import json
+
 
 def verifica_cep(ceps):
-    cep = ceps['cep'].strip()
-    email = ceps['email'].strip()
-    nome = ceps['nome'].strip()
+
+    cep = ceps["cep"]
+    email = ceps["email"].strip()
+    nome = ceps["nome"].strip()
     url = f"https://viacep.com.br/ws/{cep}/json/"
 
     try:
         response = requests.get(url, timeout=20)
         if response.status_code == 200:
             data = response.json()
-            if 'erro' in data:  # CEP inválido
-                cria_excel(cep,email, nome)
+            if "erro" in data:  # CEP inválido
+                cria_excel(
+                    "SA1", cep, email, nome, "Formato Válido, porém CEP inexistente"
+                )
         else:
-            cria_excel(cep,email, nome)
-
+            print(response)
+            cria_excel("SA1", cep, email, nome, "Formato inválido")
 
     except requests.RequestException as e:
         print(f"Erro na requisição HTTP: {e}")
 
 
-def cria_excel(cep, email, nome):
-    print('CEP -> ', cep)
-    print('EMAIL -> ', email)
-    print('NOME ->', nome)
+def cria_excel(table_name, cep, email, nome, motivo):
     filename = "ceps-errados.xlsx"
     if os.path.isfile(filename):
         workbook = openpyxl.load_workbook(filename)
@@ -36,31 +37,35 @@ def cria_excel(cep, email, nome):
     else:
         workbook = openpyxl.Workbook()
         sheet = workbook.active
-        sheet.title = "CEPs Errados"
-        
+        sheet.title = f"CEPs Errados tabela {table_name}"
+
         # Cabeçalhos
-        sheet['A1'] = "Email"
-        sheet['B1'] = "CEP"
-        sheet['C1'] = "Nome"
-        
+        sheet["A1"] = "Email"
+        sheet["B1"] = "CEP"
+        sheet["C1"] = "Nome"
+        sheet["D1"] = "Motivo"
+
         header_font = Font(bold=True)
         header_alignment = Alignment(horizontal="center")
-        header_fill = PatternFill(start_color="C8C8C8", end_color="C8C8C8", fill_type="solid")
-        
-        for col in ['A1', 'B1', 'C1']:
+        header_fill = PatternFill(
+            start_color="C8C8C8", end_color="C8C8C8", fill_type="solid"
+        )
+
+        for col in ["A1", "B1", "C1", "D1"]:
             sheet[col].font = header_font
             sheet[col].alignment = header_alignment
             sheet[col].fill = header_fill
-        
+
         last_row = 2  # Primeira linha de dados
 
     # Escreve os dados
-    sheet[f'A{last_row}'] = email
-    sheet[f'B{last_row}'] = cep
-    sheet[f'C{last_row}'] = nome
-    
+    sheet[f"A{last_row}"] = email
+    sheet[f"B{last_row}"] = cep
+    sheet[f"C{last_row}"] = nome
+    sheet[f"D{last_row}"] = motivo
+
     # Ajusta largura das colunas
-    for col in ['A', 'B', 'C']:
+    for col in ["A", "B", "C", "D"]:
         max_length = 0
         column = sheet[col]
         for cell in column:
@@ -68,6 +73,6 @@ def cria_excel(cep, email, nome):
                 max_length = max(max_length, len(str(cell.value)))
         adjusted_width = max_length + 2
         sheet.column_dimensions[col].width = adjusted_width
-    
+
     workbook.save(filename)
     workbook.close()
